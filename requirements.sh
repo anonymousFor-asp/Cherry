cace75fd4f5bb290c12f448bff5eb02ee3be19e0
anonymousFor-asp <wang-yan@stu.xidian.edu.cn>
2024-11-26 11:55:54 +0800

    init

--- requirements.sh
+++ requirements.sh
@@ -1,4 +1,4 @@
-# packages for Cherry
+#Cherry Requirements
 pip install cudatoolkit=11.8.0
 # https://pytorch.org/get-started/previous-versions/
 conda install pytorch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 pytorch-cuda=11.8 -c pytorch -c nvidia
